7a79e8f4c07e9be45743fdff13225ee1f4d4f4f9
khostaa <sunilpoudel668@gmail.com>
2023-02-04 22:39:23 +0545

    Added TOH using C

---
+++ DSA/Recursion/toh.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+void move(int n,char src,char dst,char temp)
+{
+    if(n==1)
+    {
+        printf("Move disc %d from %c to %c.\n",n,src,dst);
+    
+    }
+    else
+    {
+        move(n-1,src,temp,dst);
+        printf("Move Disc %d from %c to %c.\n",n,src,dst);
+        move(n-1,temp,dst,src);
+    }
+}
+int main()
+{
+    char a,b,t;
+    a = 'A';
+    b = 'C';
+    t = 'B';
+    int n;
+    printf("\nEnter the no. of Discs: ");
+    scanf("%d",&n);
+    printf("A - Source\nB - Temporary\nC- Destination\n");
+    move(n,a,b,t);
+    return 0;
+}
