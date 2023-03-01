55007b0c718223ff84a103459ec6d0c2018e34a3
khostaa <sunilpoudel668@gmail.com>
2023-03-01 22:46:54 +0545

    Used GitHub Copilot to make SWAP Function

--- DSA/hello.cpp
+++ DSA/hello.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
+void Swap(int *a, int *b)
+{
+
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 int main()
 {
-    cout << "Hello World!!\n";
+    cout << "Hello !\n";
     return 0;
 }
